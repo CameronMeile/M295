9a583660ff9e88b83ae51ac672e6f7f2815acb08
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-12-13 14:39:19 +0000

    [Plenum] VariableLetConst

---
+++ Plenum-varlletconst/varletconst.js
@@ -0,0 +1,14 @@
+// Variable - var
+var variable = "hello";
+variable = "Welt";
+console.log(variable);
+
+// Let variable - let
+let lett = "hello";
+lett = "Welt";
+console.log(lett);
+
+// Constante - const
+const konst = "hello";
+konst = "Welt";
+console.log(konst);
