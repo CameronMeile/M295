9f72e990641d60dc11a553e8a35cd7c45900fce5
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-12-13 09:36:16 +0000

    [Aufgabe 1.2] Updated Filtered-js

--- Aufgabe 1.2/filtered-ls.js
+++ Aufgabe 1.2/filtered-ls.js
@@ -1,9 +1,28 @@
-const fs = require('fs');
+/* const fs = require('fs')
+const path = require('path')
 
-fs.readFile(process.argv[2], FindeFile)
+fs.readdir(process.argv[2], function (err, list) {
+  if (err) {
+    console.log(err);
+  }
+  list.filter(function (file) {
+    return path.extname(file) === '.' + process.argv[3]
+  }).forEach(function (file) {
+    console.log(file);
+  })
+}) */
 
-function FindeFile (error, file) {
-    if (error) {
-        return console.log(Error);
+const fs = require('fs')
+const path = require('path')
+
+const folder = process.argv[2]
+const ext = '.' + process.argv[3]
+
+fs.readdir(folder, function (err, files) {
+  if (err) return console.error(err)
+  files.forEach(function (file) {
+    if (path.extname(file) === ext) {
+      console.log(file)
     }
-}
+  })
+})
