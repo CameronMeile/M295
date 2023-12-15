dd3cc1be715811e36515b002f406859b3899dcd3
Cam <112639963+CameronMeile@users.noreply.github.com>
2023-12-15 14:39:22 +0000

    [Aufgabe 6.2] Update

--- Aufgabe 6.2/app.js
+++ Aufgabe 6.2/app.js
@@ -1,11 +1,16 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger-output.json')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`); // Start the server and log a message indicating the port it's listening on
-});
+/* Middlewares */
+app.use(bodyParser.json())
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-//npm init
-//npm install express
-//node index.js
+app.listen(3000, () => {
+  console.log("Server is running!\nAPI documentation: http://localhost:3000/doc")
+})
+
+/* Endpoints */
+require('./src/endpoints')(app)
